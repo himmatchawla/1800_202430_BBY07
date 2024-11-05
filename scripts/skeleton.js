a0235dfb5bb08aa344542a7a675a10fc1b586743
Seonuk Baek <sbaek19@my.bcit.ca>
2024-11-04 23:30:22 -0800

    added icons for bopth before and after navbars. If user is not authenticated, user will only be able to access the index, about us, and help html page. If user is logged in and opens a new tab, it leads to main.html

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,15 +1,25 @@
 function loadSkeleton() {
     firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {                   
+        if (user) {
             // User is signed in
-            console.log("usere=s");
-            console.log($('#navbarPlaceholder').load('../text/nav_after_login.html'));
-            console.log($('#footerPlaceholder').load('../text/footer.html'));
+            console.log("User is signed in");
+            $('#navbarPlaceholder').load('../text/nav_after_login.html');
+
+            // Redirect to main page if user is on '/' or '/login.html'
+            if (window.location.pathname === '/' || window.location.pathname === '/login.html') {
+                window.location.href = '/main.html';
+            }
         } else {
             // No user is signed in
-            console.log($('#navbarPlaceholder').load('../text/nav_before_login.html'));
-            console.log($('#footerPlaceholder').load('../text/footer.html'));
+            console.log("No user is signed in");
+            $('#navbarPlaceholder').load('../text/nav_before_login.html');
+
+            // Redirect to '/' if user is not on '/' or '/login.html'
+            if (!(window.location.pathname === '/' || window.location.pathname === '/login.html'
+                || window.location.pathname === "/aboutus.html" || window.location.pathname === "/help.html")) {
+                window.location.href = '/';
+            }
         }
     });
 }
-loadSkeleton(); // call
+loadSkeleton(); // Call the function to initiate the process
