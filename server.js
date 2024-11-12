0ac5c2e3a5e70e43fbc151b0b161a5609a1b9091
Sophia Diluvio <sophiadiluvio@gmail.com>
2024-11-12 03:15:22 -0800

    updated aboutus.html

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use("/img", express.static("./public/media/bcitBurnaby"));
+
 // Setup view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
