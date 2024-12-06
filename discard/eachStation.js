a4826f1b2f3ec39bc01d85c8e1a9aa1c3c3394a3
Seonuk Baek <sbaek19@my.bcit.ca>
2024-12-06 13:07:02 -0800

    1. Deleted unused files: auraoints.html, profile.html, dropdown.js, eachStation.js, main.js, routes.js, saved.js, saved.html, saved.js, searchroutes.js, station.js

--- discard/eachStation.js
+++
@@ -1,9 +0,0 @@
-// I DO NOT KNOW WHAT THIS FILE DOES -HIMMAT
-
-function saveStationDocumentIDAndRedirect(){
-    let params = new URL(window.location.href) //get the url from the search bar
-    let ID = params.searchParams.get("docID");
-    console.log(ID);
-    localStorage.setItem('stationDocID', ID);
-    window.location.href = '../incidentReport.html';
-}
